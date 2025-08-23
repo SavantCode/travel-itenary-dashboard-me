2a9d20eca0081a459d972d2dd76de6735b3d164a
SavantCode <rahulkcoding2023@gmail.com>
2025-08-23 16:48:47 +0530

    chore: added backend API base URL configuration

--- src/api/auth_api.ts
+++ src/api/auth_api.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', // Your backend's base URL
+  // baseURL: 'http://localhost:5000/api/v1', // Your backend's base URL
+  baseURL: 'https://travelgpt-api-backend.onrender.com', // Your backend's base URL
   withCredentials: true, // This is important for cookies, if you use them
 });
 
